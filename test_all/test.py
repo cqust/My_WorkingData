e2a8b0cdb87c3912fbe09c910b9127576fc4800a
cqust <cqxiangyi@163.com>
2018-05-27 11:02:50 +0800

    test_all uploading

---
+++ test_all/test.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# # import numpy as np
+# # this is a test
+
+# In[ ]:
+
+
+import tensorflow as tf
+
+
+# In[1]:
+
+import numpy as np
+
+x = 3
+print(x)
+
+
+# In[4]:
+
+y = 20
+z = x*y
+print(z)
+
+
+# In[8]:
+
+get_ipython().magic('matplotlib inline')
+import matplotlib.pyplot as plt
+import numpy as np
+x =np.arange(20)
+y = x**2
+plt.plot(x,y)
+
+
+# 
